6ed597f95b81a051400db235496efa9098f21db7
Konstantin Aleksandrov <aleksandrovkm@gmail.com>
2019-06-07 22:38:15 +0700

    Add support for "Outdated browser module"

--- middlewares/static.js
+++ middlewares/static.js
@@ -6,7 +6,7 @@ const serve  = require('koa-static');
 
 module.exports = async (ctx, next) => {
     let staticPath = config.staticRoot;
-    let url        = path.normalize(ctx.request.url);
+    const url      = path.normalize(ctx.request.url);
 
     if (/^[\\/]{1,2}error\.pug/.test(url)) {
         return await next();
@@ -16,10 +16,15 @@ module.exports = async (ctx, next) => {
         staticPath = config.uploadsRoot;
     }
 
-    return serve(staticPath, {
+    await serve(staticPath, {
+        defer: false,
+        index: 'index.html',
         maxage : __DEV__ ? 0 : 86400000*30, // 30 days
         gzip: true,
-        usePrecompiledGzip: true,
         hidden: false
     })(ctx, next);
+
+    if (ctx.body && 'text/html' === ctx.type && 'GET' === ctx.method) { // Send to outdated browser check
+        await next();
+    }
 };
